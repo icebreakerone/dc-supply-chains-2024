2dbef1f557cce753b2b7c52163ba3e004863586f
Ben Summers <ben.summers@ib1.org>
2024-12-11 17:50:37 +0000

    Modify Perseus record for supply chain

--- main.py
+++ main.py
@@ -15,25 +15,12 @@ TRUST_FRAMEWORK_URL = "https://registry.core.trust.ib1.org/trust-framework"
 
 
 if __name__ == "__main__":
-    # Create a mechanism to provide certificates for verification. Multiple
-    # implementations: embed certificates in record, entirely local, fetch from
-    # Directory with local caching, etc.
-    # Implements certificate policy, including whether certificates are included
-    # in the record.
-    # Provides the signing CA root certificate. Each environment will have its own
-    # root CA.
-    # Use certificates from the record, with policy to include them when adding steps
+    # Certificate provider
     certificate_provider = CertificatesProviderSelfContainedRecord(
         "certs/4-signing-ca-cert.pem"
     )
 
-    # Create signers representing two applications to illustrate a record
-    # passed between two members. (In a real application, you'd only have one.)
-    # A few signer classes should be provided, eg local PEM files like this one,
-    # or a key stored in AWS' key service.
-    # Uses a Certificate Provider object for access to certificate policy.
-    # Energy Data Provider Signer with certificates and key stored in files:
-
+    # Load signers
     signers = {}
     signer_file_list = [
         "6-industrial-metering-company",
@@ -51,221 +38,179 @@ if __name__ == "__main__":
                 "certs/"+n+"-cert-bundle.pem",
                 "certs/"+n+"-key.pem"
             )
-    signer_edp = signers['6-industrial-metering-company']
-    signer_cap = signers['12-sustainable-farm-systems']
-    signer_bank = signers['13-green-bank-of-london']
 
     # -----------------------------------------------------------------------
-    # ===== EDP starts a new Record, fetching Smart Meter data
-    edp_record = Record(TRUST_FRAMEWORK_URL)
+    # ===== Metering provider
+    metering_record = Record(TRUST_FRAMEWORK_URL)
     # - Permission step to record consent by end user
-    edp_permission_id = edp_record.add_step(
+    metering_permission_id = metering_record.add_step(
         {
             "type": "permission",
-            "scheme": "https://registry.core.trust.ib1.org/scheme/perseus",
+            "scheme": "https://registry.core.trust.ib1.org/scheme/supply",
             "timestamp": "2024-09-20T12:16:11Z",    # granted in past; must match audit trail
             "account": "/yl4Y/aV6b80fo5cnmuDDByfuEA=",
             "allows": {
                 "licences": [
-                    "https://smartenergycodecompany.co.uk/documents/sec/consolidated-sec/",
-                    "https://registry.core.trust.ib1.org/scheme/perseus/licence/energy-consumption-data/2024-12-05"
+                    "https://registry.core.trust.ib1.org/scheme/supply/licence/metered-supply-data/2024-12-05"
                 ]
             },
             "expires": "2025-09-20T12:16:11Z"       # 1 year
         }
     )
     # - Origin step for the smart meter data
-    origin_id = edp_record.add_step(
+    origin_id = metering_record.add_step(
         {
             "type": "origin",
-            "scheme": "https://registry.core.trust.ib1.org/scheme/perseus",
-            "sourceType": "https://registry.core.trust.ib1.org/scheme/perseus/source-type/Meter",
-            "origin": "https://www.smartdcc.co.uk/",
-            "originLicence": "https://smartenergycodecompany.co.uk/documents/sec/consolidated-sec/",
-            "external": True,
-            "permissions": [edp_permission_id],
-            "perseus:scheme": {
+            "scheme": "https://registry.core.trust.ib1.org/scheme/supply",
+            "sourceType": "https://registry.core.trust.ib1.org/scheme/supply/source-type/Meter",
+            "origin": "https://industrialmetering.example.com/",
+            "external": False,
+            "supply:scheme": {
                 "meteringPeriod": {
-                    "from": "2023-09-01Z",
+                    "from": "2024-08-01Z",
                     "to": "2024-09-01Z"
                 }
             },
-            "perseus:assurance": {
-                "missingData": "https://registry.core.trust.ib1.org/scheme/perseus/assurance/missing-data/Missing",
-                "originMethod": "https://registry.core.trust.ib1.org/scheme/perseus/assurance/origin-method/SmartDCCOtherUser"
+            "supply:assurance": {
+                "missingData": "https://registry.core.trust.ib1.org/scheme/supply/assurance/missing-data/Missing",
+                "materialQuantity": "https://registry.core.trust.ib1.org/scheme/supply/assurance/material-quantity/Measured"
             }
         }
     )
     # - Transfer step to send it to the CAP
-    edp_record.add_step(
+    metering_transfer_step_id = metering_record.add_step(
         {
             "type": "transfer",
-            "scheme": "https://registry.core.trust.ib1.org/scheme/perseus",
+            "scheme": "https://registry.core.trust.ib1.org/scheme/supply",
             "of": origin_id,
-            "to": "https://directory.core.trust.ib1.org/member/81524", # CAP
-            "standard": "https://registry.core.trust.ib1.org/scheme/perseus/standard/energy-consumption-data/2024-12-05",
-            "licence": "https://registry.core.trust.ib1.org/scheme/perseus/licence/energy-consumption-data/2024-12-05",
-            "service": "https://api.honestdave.example.com/meter-readings/0",
+            "to": "https://directory.core.trust.ib1.org/member/293482",
+            "standard": "https://registry.core.trust.ib1.org/scheme/supply/standard/metered-supply-data/2024-12-05",
+            "licence": "https://registry.core.trust.ib1.org/scheme/supply/licence/metered-supply-data/2024-12-05",
+            "service": "https://api.industrialmetering.example.com/meter-readings/0",
             "path": "/readings",
             "parameters": {
-                "measure": "import",
-                "from": "2023-09-01Z",
+                "from": "2024-08-01Z",
                 "to": "2024-09-01Z"
             },
-            "permissions": [edp_permission_id],
+            "permissions": [metering_permission_id],
             "transaction": "C25D0B85-B7C4-4543-B058-7DA57B8D9A24",
         }
     )
-    # EDP signs the steps
-    edp_record_signed = edp_record.sign(signer_edp)
+    # Metering provider signs the steps
+    metering_record_signed = metering_record.sign(signers['6-industrial-metering-company'])
     # Get encoded data for inclusion in data response
-    edp_data_attachment = edp_record_signed.encoded()
+    metering_data_attachment = metering_record_signed.encoded()
 
     # -----------------------------------------------------------------------
     # ===== CAP retrieves the data from the EDP, who includes a provenance record in the response
-    cap_record = Record(TRUST_FRAMEWORK_URL, edp_data_attachment)
+    manufacturer_record = Record(TRUST_FRAMEWORK_URL, metering_data_attachment)
     # - Verify the signatures on the record
-    cap_record.verify(certificate_provider)
-    # - Find the transfer step, passing in the expected values for the transfer step (exceptions if not found)
-    transfer_from_edp_step = cap_record.find_step(
-        {
-            # Same values as the transfer step added by the EDP
-            "type": "transfer",
-            "scheme": "https://registry.core.trust.ib1.org/scheme/perseus",
-            "to": "https://directory.core.trust.ib1.org/member/81524", # CAP
-            "standard": "https://registry.core.trust.ib1.org/scheme/perseus/standard/energy-consumption-data/2024-12-05",
-            "licence": "https://registry.core.trust.ib1.org/scheme/perseus/licence/energy-consumption-data/2024-12-05",
-            "service": "https://api.honestdave.example.com/meter-readings/0",
-            "path": "/readings",
-            "parameters": {
-                "measure": "import",
-                "from": "2023-09-01Z",
-                "to": "2024-09-01Z"
-            }
-        }
-    )
+    manufacturer_record.verify(certificate_provider)
     # - Add a receipt step
-    cap_receipt_id = cap_record.add_step(
+    manufacturer_receipt_id = manufacturer_record.add_step(
         {
             "type": "receipt",
-            "transfer": transfer_from_edp_step["id"]
+            "transfer": metering_transfer_step_id
         }
     )
     # - Permission step to record consent to processing and future transfer
-    cap_permission_id = cap_record.add_step(
+    manufacturer_permission_id = manufacturer_record.add_step(
         {
             "type": "permission",
-            "scheme": "https://registry.core.trust.ib1.org/scheme/perseus",
-            "timestamp": "2024-10-21T09:09:10Z",    # granted in past; must match audit trail
-            "account": "dbd16978-a0a642d9aa2d95318b50e605", # different to EDP as this is the CAP's account
+            "scheme": "https://registry.core.trust.ib1.org/scheme/supply",
+            "timestamp": "2024-10-21T09:09:10Z",
+            "account": "dbd16978-a0a642d9aa2d95318b50e605",
             "allows": {
                 "licences": [
-                    "https://registry.core.trust.ib1.org/scheme/perseus/licence/emissions-report/2024-12-05"
+                    "https://registry.core.trust.ib1.org/scheme/supply/licence/supply-data/2024-12-05"
                 ],
                 "processes": [
-                    "https://registry.core.trust.ib1.org/scheme/perseus/process/emissions-calculations/2024-12-05"
+                    "https://registry.core.trust.ib1.org/scheme/supply/process/manufacture/2024-12-05"
                 ]
             },
             "expires": "2025-10-21T09:09:10Z"       # 1 year
         }
     )
     # - Add an origin step for grid intensity data
-    cap_intensity_origin_id = cap_record.add_step(
+    manufacturer_batch_origin_id = manufacturer_record.add_step(
         {
             "type": "origin",
-            "scheme": "https://registry.core.trust.ib1.org/scheme/perseus",
-            "sourceType": "https://registry.core.trust.ib1.org/scheme/perseus/source-type/GridCarbonIntensity",
+            "scheme": "https://registry.core.trust.ib1.org/scheme/supply",
+            "sourceType": "https://registry.core.trust.ib1.org/scheme/supply/source-type/GridCarbonIntensity",
             "origin": "https://api.carbonintensity.org.uk/",
             "originLicence": "https://creativecommons.org/licenses/by/4.0/",
             "external": True,
-            "perseus:scheme": {
+            "supply:scheme": {
                 "meteringPeriod": {
                     "from": "2023-09-01Z",
                     "to": "2024-09-01Z"
                 },
                 "postcode": "CF99"
             },
-            "perseus:assurance": {
-                "missingData": "https://registry.core.trust.ib1.org/scheme/perseus/assurance/missing-data/Complete"
+            "supply:assurance": {
+                "missingData": "https://registry.core.trust.ib1.org/scheme/supply/assurance/missing-data/Complete"
             }
         }
     )
     # - Add a process step to combine the data from the EDP and the grid intensity API into the report
-    cap_processing_id = cap_record.add_step(
+    manufacturer_processing_id = manufacturer_record.add_step(
         {
             "type": "process",
-            "scheme": "https://registry.core.trust.ib1.org/scheme/perseus",
+            "scheme": "https://registry.core.trust.ib1.org/scheme/supply",
             "inputs": [
-                cap_receipt_id,
-                cap_intensity_origin_id
+                manufacturer_receipt_id,
+                manufacturer_batch_origin_id
             ],
-            "process": "https://registry.core.trust.ib1.org/scheme/perseus/process/emissions-calculations/2024-12-05",
-            "permissions": [cap_permission_id],
-            "perseus:assurance": {
-                "missingData": "https://registry.core.trust.ib1.org/scheme/perseus/assurance/missing-data/Substituted"
+            "process": "https://registry.core.trust.ib1.org/scheme/supply/process/manufacture/2024-12-05",
+            "permissions": [manufacturer_permission_id],
+            "supply:assurance": {
+                "missingData": "https://registry.core.trust.ib1.org/scheme/supply/assurance/missing-data/Substituted",
+                "audit": "https://registry.core.trust.ib1.org/scheme/supply/audit-standard/FS128983"
             }
         }
     )
-    # - Add a transfer step to send it to the bank
-    cap_record.add_step(
+    # - Add a transfer step to send it to the wholesaler
+    manufacturer_transfer_step_id = manufacturer_record.add_step(
         {
             "type": "transfer",
-            "scheme": "https://registry.core.trust.ib1.org/scheme/perseus",
-            "of": cap_processing_id,
-            "to": "https://directory.core.trust.ib1.org/member/71212388", # Bank
-            "standard": "https://registry.core.trust.ib1.org/scheme/perseus/standard/emissions-report/2024-12-05",
-            "licence": "https://registry.core.trust.ib1.org/scheme/perseus/licence/emissions-report/2024-12-05",
+            "scheme": "https://registry.core.trust.ib1.org/scheme/supply",
+            "of": manufacturer_processing_id,
+            "to": "https://directory.core.trust.ib1.org/member/927625",
+            "standard": "https://registry.core.trust.ib1.org/scheme/supply/standard/supply-data/2024-12-05",
+            "licence": "https://registry.core.trust.ib1.org/scheme/supply/licence/supply-data/2024-12-05",
             "service": "https://api.emmissions4u.example.com/emission-report/23",
-            "path": "/emissions",
+            "path": "/supply",
             "parameters": {
-                "from": "2023-09Z",
-                "to": "2024-09Z"
+                "invoiceNumber": "F2928-282847"
             },
-            "permissions": [cap_permission_id],
+            "permissions": [manufacturer_permission_id],
             "transaction": "C5813265-515B-48DC-925F-832FA418F7E2"
         }
     )
 
     # CAP signs the steps
-    cap_record_signed = cap_record.sign(signer_cap)
+    manufacturer_record_signed = manufacturer_record.sign(signers['7-nitrogen-fertiliser-products'])
     # Get encoded data for inclusion in data response
-    cap_data_attachment = cap_record_signed.encoded()
+    manufacturer_data_attachment = manufacturer_record_signed.encoded()
 
     # -----------------------------------------------------------------------
-    # ===== Bank retrieves the data from the CAP, who includes a provenance record in the response
-    bank_record = Record(TRUST_FRAMEWORK_URL, cap_data_attachment)
+    # ===== Wholesaler
+    wholesaler_record = Record(TRUST_FRAMEWORK_URL, manufacturer_data_attachment)
     # - Verify the signatures on the record
-    bank_record.verify(certificate_provider)
-    # - Find the transfer step, passing in the expected values for the transfer step (exceptions if not found)
-    transfer_from_cap_step = bank_record.find_step(
-        {
-            # Same values as the transfer step added by the CAP
-            "type": "transfer",
-            "scheme": "https://registry.core.trust.ib1.org/scheme/perseus",
-            "to": "https://directory.core.trust.ib1.org/member/71212388", # Bank
-            "standard": "https://registry.core.trust.ib1.org/scheme/perseus/standard/emissions-report/2024-12-05",
-            "licence": "https://registry.core.trust.ib1.org/scheme/perseus/licence/emissions-report/2024-12-05",
-            "service": "https://api.emmissions4u.example.com/emission-report/23",
-            "path": "/emissions",
-            "parameters": {
-                "from": "2023-09Z",
-                "to": "2024-09Z"
-            }
-        }
-    )
+    wholesaler_record.verify(certificate_provider)
     # - Add a receipt step
-    bank_receipt_id = bank_record.add_step(
+    wholesaler_receipt_id = wholesaler_record.add_step(
         {
             "type": "receipt",
-            "transfer": transfer_from_cap_step["id"]
+            "transfer": manufacturer_transfer_step_id
         }
     )
 
     # Bank signs the steps
-    bank_record_signed = bank_record.sign(signer_bank)
+    wholesaler_record_signed = wholesaler_record.sign(signers["8-agricultural-wholesale-supplies"])
 
     # ===== Final record after all the the steps have been added
-    final_record = bank_record_signed
+    final_record = wholesaler_record_signed
 
     # Print records
     print("----- Record (encoded for transfer) -----")
